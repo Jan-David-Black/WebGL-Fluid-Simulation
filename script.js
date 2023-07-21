eb139877157dedd042ffc8dd960e8d3bf04c52c1
Jan David Fischbach <92856893+Jan-David-Black@users.noreply.github.com>
2023-07-20 21:54:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1563,7 +1563,7 @@ function correctDeltaY (delta) {
 }
 
 function generateColor () {
-    let c = HSVtoRGB(Math.random(), 1.0, 1.0);
+    let c = HSVtoRGB(0, 1.0, 1.0); //Math.random()
     c.r *= 0.15;
     c.g *= 0.15;
     c.b *= 0.15;
